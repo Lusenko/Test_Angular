ead245ab742c18f22ccf3467866a43892860047e
Oleksandr Lusenko <lusenko.sasha.2015@gmail.com>
2021-08-18 15:17:01 +0300

    made service for 4 tutorial

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Hero} from "../hero";
-import {HEROES} from '../mock-heroes';
+import {HeroService} from "../hero.service";
 
 @Component({
   selector: 'app-heroes',
@@ -9,16 +9,22 @@ import {HEROES} from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
+  public heroes?: Hero[];
 
-  selectedHero?: Hero;
+  public selectedHero?: Hero;
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
+
+  getHeroes(): void{
+    this.heroes = this.heroService.getHeroes();
+  }
 
   ngOnInit(): void {
+    this.getHeroes();
   }
 
 }
